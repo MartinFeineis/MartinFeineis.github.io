77b3f82e79c9fb392a6f68b270b873c87cdcea98
Martin Feineis <martifein@gmail.com>
2023-05-22 16:53:07 -0400

    porting vue over

---
+++ hmy-page/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Jobs from './components/Jobs.vue'
+import Profile from './components/Profile.vue'
+import SendMessage from './components/SendMsg.vue'
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header,
+    Profile,
+    SendMessage,
+    Jobs
+  }
+}
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Profile />
+    <SendMessage to:martinfeineis/>
+    <Jobs />
+  </div>
+</template>
---
+++ hmy-page/components/Profile.vue
@@ -0,0 +1,56 @@
+<script>
+
+export default{
+    data(){
+        return{
+            Name: "Martin Feineis",
+            Description: "I have 15 years experience how not to become a DevOps Engineer. Since then I got mentored a lot and also mentored others on their journey to level up their IT Experience.",
+            pictures: [
+              {
+              id: "1",
+              src: "/picture1.jpeg",
+              isActive: true,
+              altText: "Celebrating Open Source Techonolgy for Enterprise"
+              },
+              {
+              id: "2",
+              src: "/picture2.jpeg",
+              isActive: false,
+              altText: "Bewerbungsfoto"
+              },
+              {
+              id: "3",
+              src: "/picture3.jpeg",
+              isActive: false,
+              altText: "Professional picture from company conference"
+              }
+            ]
+        }
+    }
+}
+</script>
+<template>
+  <div class="row g-0">
+    <div class="col-md-4">
+    <div id="carouselExample" class="carousel slide ">
+  <div class="carousel-inner">
+    <div v-for="picture in pictures" :key='picture.id' class="carousel-item" :class="{active : picture.isActive}">
+      <img :src="picture.src" class="img-thumbnail d-block w-100" :alt="picture.altText">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+  </div>
+</div>
+  <div class="col-md-8">
+    <h1>{{ Name }}</h1>
+    <p>{{ Description }}</p>
+  </div>
+</div>
+</template>
