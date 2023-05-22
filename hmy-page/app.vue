7c66f8caf7a38e805a64bb90a8fa040399a64cc7
Martin Feineis <martifein@gmail.com>
2023-05-22 16:53:30 -0400

    removing old vue

--- hmy-page/app.vue
+++ hmy-page/app.vue
@@ -1,5 +1,23 @@
+<script>
+import Jobs from './components/Jobs.vue'
+import Profile from './components/Profile.vue'
+import SendMessage from './components/SendMsg.vue'
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header,
+    Profile,
+    SendMessage,
+    Jobs
+  }
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <Header />
+    <Profile />
+    <SendMessage to:martinfeineis/>
+    <Jobs />
   </div>
 </template>
