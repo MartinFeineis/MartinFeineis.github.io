e8d289e0935666a96856dcb6774fc4ab7c7b777d
Martin Feineis <martifein@gmail.com>
2023-05-24 21:37:09 +0000

    updates

--- hmy-page/pages/message.vue
+++ hmy-page/pages/message.vue
@@ -15,7 +15,7 @@
   <body>
     <div>
       <Header />
-      <SendMessage />
+      <SendMsg />
     </div>
     <script type="module" src="/nested/src/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
