7a000c1c762b07d8984af3906f3c463a00348fd5
Martin Feineis <martifein@gmail.com>
2023-06-03 07:28:10 -0400

    adding mortgage page

--- hmy-page/components/Header.vue
+++ hmy-page/components/Header.vue
@@ -17,6 +17,9 @@ export default {
         <li class="nav-item">
           <a class="nav-link" href="/message">Messaging Service</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/mortgage">Mortgage calculator</a>
+        </li>
         <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
