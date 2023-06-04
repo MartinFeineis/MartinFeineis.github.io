c3375e968d79b7ad3ed41e788747769ee2fc94f5
MartinFeineis <martifein@gmail.com>
2023-06-04 23:46:55 +0000

    Deploying to gh-pages from @ MartinFeineis/MartinFeineis.github.io@7cc62bd4a2df872d01ac7a66a2d5aa4d19f535f1 🚀

--- hmy-page/.nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = string
-declare module "/workspaces/MartinFeineis.github.io/hmy-page/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
