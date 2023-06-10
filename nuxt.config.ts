760dd6594ef97bd48cebe19ddc27b8208cbbd96e
Martin Feineis <martifein@gmail.com>
2023-06-10 12:23:45 -0400

    with bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css'
+  ],
+  script: [
+    {
+      src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
+    }
+  ],
+  vite: {
+    define: {
+      "process.env.DEBUG": false,
+    },
+  },
+});
