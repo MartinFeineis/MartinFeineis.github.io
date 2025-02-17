0d39276c83f417aeee044ca7bb262a5432b530a4
Martin Feineis <martifein@gmail.com>
2025-02-16 19:43:47 -0500

    adding github stats

--- script.js
+++ script.js
@@ -28,6 +28,19 @@ async function loadResume() {
         `;
         resumeDiv.appendChild(header);
 
+        // Github Stats
+        const ghstat = document.createElement("div");
+        ghstat.classList.add("section");
+        ghstat.innerHTML = `
+            <a href="https://github.com/martinfeineis/github-readme-stats">
+                <img height=200 align="center" src="https://github-readme-stats.vercel.app/api?username=martinfeineis" />
+            </a>
+            <a href="https://github.com/martinfeineis/convoychat">
+                <img height=200 align="center" src="https://github-readme-stats.vercel.app/api/top-langs?username=martinfeineis&layout=compact&langs_count=8&card_width=320" />
+            </a>
+        `;
+        resumeDiv.appendChild(ghstat);
+
         // Skills
         const skills = document.createElement("div");
         skills.classList.add("section");
